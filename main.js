777ff69c7657555372f167299b3b426aa0a4b388
Dasom Ahn <lizzy0521@naver.com>
2022-09-24 02:45:32 +0900

    코드 해석

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let UserVideo = document.querySelector("#localVideo");
 let partnerVideo = document.querySelector("#remoteVideo");
 let call_container = document.querySelector("#call_container");
 let incoming_call = document.querySelector("#incoming_call");
+let myInfo = document.querySelector("#myInfo");
 let yourID;
 let candidate_to_add;
 let otherPerson;
@@ -15,7 +16,15 @@ let pc = new RTCPeerConnection({
 
 // setup pc for webRTC
 pc.onicecandidate = (e) => {
+  console.log("이 작업은 언제 되는걸까?")
+  // call을 누르니까 되네?
+  // callee는 answer 누르니까 뜬다
+  // 찾아보니까 이 onIceCandidate는
+  // setLocalDescription 할때 일어난대
+
+  console.log(e);
   if (e.candidate) {
+    console.log("otherPerson ", otherPerson);
     if (otherPerson == yourID) {
       console.log("shouldn't send any here");
     } else {
@@ -50,8 +59,9 @@ navigator.mediaDevices
   });
 
 // call a user
-function createOffer(person_to_call) {
-  pc.createOffer({
+function call_user(person_to_call) {
+  pc.createOffer({ // callee에게 전달할 sdp 생성
+    // session description protocol로 미디어 정보를 교환
     mandatory: {
       offerToReceiveAudio: false,
       offerToReceiveVideo: true,
@@ -64,7 +74,7 @@ function createOffer(person_to_call) {
         from: yourID,
       });
 
-      pc.setLocalDescription(sdp);
+      pc.setLocalDescription(sdp); // 로컬 sdp로 설정
     },
     (e) => {}
   );
@@ -92,17 +102,19 @@ function createAnswer(guy_I_accepted_call_from) {
       });
 
       pc.setLocalDescription(sdp);
+      // callee도 자기 sdp local에 설정
     },
     (e) => {}
   );
 }
 
-function call_user(person_to_call) {
-  createOffer(person_to_call);
-}
+// function call_user(person_to_call) {
+//   createOffer(person_to_call);
+// }
 
 socket.on("yourID", (id) => {
   yourID = id;
+  myInfo.innerHTML=`${id}`
 });
 
 // when call -> do for everyone else
@@ -111,9 +123,14 @@ socket.on("allUsers", (users) => {
   let all_users = Object.keys(users);
 
   all_users.forEach((user_id) => {
-    call_container.innerHTML += `
-    <button class="call_button" data-person_to_call=${user_id}>Call</button>
-    `;
+    if (user_id == yourID) {
+      // nothing
+    }
+    else {
+      call_container.innerHTML += `
+      <button class="call_button" data-person_to_call=${user_id}>Call ${user_id}</button>
+      `;
+    }
   });
   let call_buttons = document.querySelectorAll(".call_button");
   call_buttons.forEach((call_button) => {
@@ -126,13 +143,16 @@ socket.on("allUsers", (users) => {
 // when accept call
 function acceptCall(sdp, guy_I_accepted_call_from) {
   setRemoteDescription(sdp);
+  // callee는 이 정보를 remote로 설정
   createAnswer(guy_I_accepted_call_from);
+  // 이 candidate는 어디서 튀어나온건지
   const candidate = JSON.parse(candidate_to_add);
   pc.addIceCandidate(new RTCIceCandidate(candidate));
 }
 
 //recv from other
 socket.on("recv", (data) => {
+  console.log("이 recv는 언제 실행되는겨")
   if (data.id == yourID) {
     otherPerson = data.id;
   } else {
@@ -145,6 +165,7 @@ socket.on("recv", (data) => {
 socket.on("hey", (data) => {
   incoming_call.innerHTML = `
     <h1>Someone is calling you</h1>
+    <p>someone is ${data.from}</p>
     <div>
       <button id="answer">Answer</button>
       <button id="decline">Decline</button>
@@ -156,13 +177,18 @@ socket.on("hey", (data) => {
 
   answer.addEventListener("click", (e) => {
     acceptCall(data.sdp, data.from);
+    incoming_call.innerHTML = "";
   });
   decline.addEventListener("click", (e) => {
     console.log("Declined call");
+    // decline 로직 없음
+    // 일단 그냥 사라지는거로 표기
+    incoming_call.innerHTML = "";
   });
 });
 
 socket.on("callAccepted", (data) => {
+  // caller은 callee의 sdp를 자신의 remote로 설정
   pc.setRemoteDescription(JSON.parse(data.sdp));
 });
 
--- server.js
+++ server.js
@@ -17,13 +17,23 @@ let io = socket(server);
 const users = {};
 let candidate = [];
 
-io.on("connection", function (socket) {
+io.on("connection", function (socket) { // connection이 들어올 때 마다
   if (!users[socket.id]) {
     users[socket.id] = socket.id;
   }
   socket.emit("yourID", socket.id);
 
   io.sockets.emit("allUsers", users);
+  /* 
+    main.js의 "allUsers"에서 일어나는 일
+    1. users 배열을 받는다
+    2. users의 user_id마다 call 버튼 생성
+    (자기 자신 포함)
+    3. call_buttons 각각에 대해 클릭했을 시
+    main.js의 call_user(전화 걸 사람) 함수 호출
+    // 4. call_user(걸사람) -> createOffer(걸사람) 삭제
+    4. sdp 생성 후 "callUser"로 emit해서 전달
+  */
 
   socket.on("disconnect", () => {
     console.log("disconnected");
@@ -34,6 +44,7 @@ io.on("connection", function (socket) {
 
   socket.on("callUser", (data) => {
     io.to(data.userToCall).emit("hey", { sdp: data.sdp, from: data.from });
+    // 특정 socket(userToCall)한테 전달?
   });
 
   socket.on("acceptedCall", (data) => {
@@ -45,7 +56,7 @@ io.on("connection", function (socket) {
   socket.on("candidate", (data) => {
     candidate.push(data.candidate);
 
-    if (candidate.length < 2) {
+    if (candidate.length < 2) { // ==1
       io.sockets.emit("recv", { candidate: candidate[0], id: socket.id });
     } else {
     }
