9ebcd13e1f36ba4619c400ddea619b43f0c41461
Dasom Ahn <lizzy0521@naver.com>
2022-10-01 05:10:23 +0900

    #1 서버 구축 (6/8)

--- new.js
+++ new.js
@@ -1,4 +1,4 @@
-let socket = io.connect("http://localhost:4000");
+let socket; // = io.connect("http://localhost:4000");
 let peers = {}
 let optionsRTC = {
     configuration: {
@@ -7,7 +7,6 @@ let optionsRTC = {
     }
 }
 let userVideo = document.querySelector("#localVideo");
-let videoContainer = document.querySelector("#video_container")
 let localStream = null;
 
 
@@ -20,19 +19,25 @@ let displayMediaOptions = {
 };
 
 const success = (stream) => {
-    userVideo.srcObject = stream;
-    localStream = stream;
-  
-    init();
+   
 };
 
 navigator.mediaDevices
   .getDisplayMedia(displayMediaOptions) // 화면 공유 옵션
-  .then(success)
+//   .then(success)
+    .then ( stream => {
+        userVideo.srcObject = stream;
+        localStream = stream;
+      
+        init();
+    })
   .catch(() => {
     console.log("errors with the media device");
 });
 
+// redirect if not https
+// if(location.href.substr(0,5) !== 'https') 
+//     location.href = 'https' + location.href.substr(4, location.href.length - 4)
 
 ////////////// CONFIG //////////////
 const configRTC = {
@@ -56,13 +61,22 @@ const configRTC = {
 function init() {
     socket = io()
 
+    socket.on('getInfo', socket_id => {
+        let info = document.getElementById("myInfo");
+        info.innerHTML += ' '+socket_id;
+    })
+
+    // initReceieve
     socket.on('newUserArr', socket_id => {
         console.log(socket_id + " = new!");
         addPeer(socket_id, false);
-
+        console.log("여기를 안하니?")   
         socket.emit('sayHiToNewbie', socket_id);
+        console.log(" 하는데...")
+        //initsenddddd
     })
 
+    // on initsend
     socket.on('newbieSaysThx', socket_id => {
         console.log("newbie thanks ", socket_id);
         addPeer(socket_id, true);
@@ -77,6 +91,14 @@ function init() {
     });
 }
 
+/**
+ * Creates a new peer connection and sets the event listeners
+ * @param {String} id 
+ *                 ID of the peer
+ * @param {Boolean} isInit
+ *                  Set to true if the peer initiates the connection process.
+ *                  Set to false if the peer receives the connection. 
+ */
 function addPeer(id, isInit) {
     peers[id] = new SimplePeer({
         initiator: isInit,
@@ -85,16 +107,19 @@ function addPeer(id, isInit) {
     });
 
     peers[id].on('signal', data => {
-        socket.emit('signal', {
+        console.log("해줘 제발")
+        socket.emit('signala', {
             signal: data,
             socket_id: id
         })
     });
 
     peers[id].on('stream', stream => {
+        let videoContainer = document.querySelector("#video_container")
         let newVideo = document.createElement('video');
         newVideo.srcObject = stream;
         newVideo.id = id;
         videoContainer.appendChild(newVideo);
     });
+
 }
--- newserver.js
+++ newserver.js
@@ -1,48 +1,67 @@
-let express = require("express");
-let socket = require("socket.io");
+const fs = require('fs')
 const path = require('path')
+const express = require('express')
+const app = express()
+const httpolyglot = require('httpolyglot')
+const httpsServer = httpolyglot.createServer({key: '',
+cert: ''}, app);
+const port = process.env.PORT || 4000
 
 //App setup
-let app = express();
-app.set('port', process.env.PORT || 4000);
-let server = app.listen(app.get('port'), function () {
-  console.log("listening to requests on port 4000");
-});
+const io = require('socket.io')(httpsServer)
+
+httpsServer.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname,'node_modules')));
 
 var router = express.Router();
-router.route('/').get(function(req, res) {
-    res.redirect('./video_chat.html'); // 상대경로
-});
-app.use('/', router);
+    router.route('/').get(function(req, res) {
+        res.redirect('./video_chat.html'); // 상대경로
+    });
+    app.use('/', router);
+    
+    app.all('*', function(req, res){
+        res.status(404).send('<h1>error-잘못된 접근입니다</h1>')
+        // 404 에러 반환 + send msg
+    })
 
-app.all('*', function(req, res){
-    res.status(404).send('<h1>error-잘못된 접근입니다</h1>')
-    // 404 에러 반환 + send msg
-})
-
-//socket setup
-let io = socket(server);
 
 /////////////////////////////
-const peers = {};
+let peers = {};
 
-io.on("connection", function (socket) {
+io.on("connect", (socket) => {
   console.log("new");
   
   peers[socket.id] = socket;
+  socket.emit('getInfo', socket.id);
 
   // setup new peer connection
   for (let id in peers) {
-    if(id == socket.id) continue;
+    if(id === socket.id) continue;
 
     peers[id].emit('newUserArr', socket.id)
+    // emit initReceive
+    console.log("이거ㅗ 하잖아");
   }
 
-  socket.on('sayHiToNewBie', new_id => {
+  //(initSenddd) emit initsend
+  socket.on('sayHiToNewbie', new_id => {
     console.log(socket.id + " said hi to " + new_id);
     peers[new_id].emit('newbieSaysThx', socket.id)
+  })
+
+  // socket for transfering signals in the middle
+  socket.on("signala", data => {
+    console.log(socket.id, "->", data.socket_id);
+    if(!peers[data.socket_id]) {
+      console.log("maybe for disconnections");
+    }
+    peers[data.socket_id].emit('signal', {
+      socket_id: socket.id,
+      signal: data.signal
+    })
   })
 });
