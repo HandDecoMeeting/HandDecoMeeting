f6cf2dc5c9c5908ec65f77f70fa35138f52a8052
Dasom Ahn <lizzy0521@naver.com>
2022-10-28 17:31:41 +0900

    #6 웹캠 연결 (진행중)

--- test.py
+++ test.py
@@ -1,5 +1,27 @@
+import cv2
+from js import document, setInterval, \
+  setTimeout, ImageData, Uint8ClampedArray, \
+  CanvasRenderingContext2D as Context2d
+from pyodide.ffi import create_proxy, to_js
+import asyncio
+import gc
 import numpy as np
-def make_x_and_y(n):
-    x = np.random.randn(n)
-    y = np.random.randn(n)
-    return x, y
+
+def draw_image(ctx, x, y, image):
+    data = Uint8ClampedArray.new(to_js(image.tobytes()))
+    height, width, _ = image.shape
+    image_data = ImageData.new(data, width, height)
+    ctx.putImageData(image_data, 0, 0, x, y, width, height)
+
+def ShowFrame(cap, frnum):
+    # global cap, frnum
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frnum)
+    ret, frame = cap.read() # 두 개의 값을 반환하므로 두 변수 지정
+    
+    reverse = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    
+    draw_image(ctx, 0, 0, reverse)
+    document.getElementById('frameNo').innerHTML=frnum
+    frnum=frnum+10
+    gc.collect()
+    setTimeout(ShowFrameProxy, 0)
