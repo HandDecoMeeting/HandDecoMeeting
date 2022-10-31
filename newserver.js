0778a8f7ad8a7d5a44df3a7bb90714b5b2075ffe
Dasom Ahn <lizzy0521@naver.com>
2022-10-31 17:44:26 +0900

    #8 서버 수정

--- newserver.js
+++ newserver.js
@@ -1,21 +1,18 @@
-const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const app = express()
-const httpolyglot = require('httpolyglot')
-const httpsServer = httpolyglot.createServer({key: '',
-cert: ''}, app);
 const port = process.env.PORT || 4000
-
-//App setup
-const io = require('socket.io')(httpsServer)
-
-httpsServer.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname,'node_modules')));
+app.set('port', port);
+
+let server = app.listen(app.get('port'), function () {
+  console.log(`listening on port ${port}`)
+})
+
+//App setup
+let io = require('socket.io')(server)
 
 var router = express.Router();
     router.route('/').get(function(req, res) {
